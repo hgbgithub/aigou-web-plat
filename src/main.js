425a49e4053ab32bd3adf921f078d301220ac170
hgbgithub <408079493@qq.com>
2019-07-29 22:30:03 +0800

    更改模块

--- src/main.js
+++ src/main.js
@@ -10,9 +10,20 @@ import Vuex from 'vuex'
 //import NProgress from 'nprogress'
 //import 'nprogress/nprogress.css'
 import routes from './routes'
-import Mock from './mock'
-Mock.bootstrap();
+//禁用mock
+//import Mock from './mock'
+//Mock.bootstrap();
 import 'font-awesome/css/font-awesome.min.css'
+import axios from 'axios'
+
+
+// 引用axios，并设置基础URL为后端服务api地址
+axios.defaults.baseURL = 'http://localhost:2000/services'  //对应后端网关统一地址
+// 将API方法绑定到全局  /plat/login
+Vue.prototype.$http = axios
+Vue.config.productionTip = false
+
+
 
 Vue.use(ElementUI)
 Vue.use(VueRouter)
@@ -36,6 +47,9 @@ router.beforeEach((to, from, next) => {
     next()
   }
 })
+
+
+
 
 //router.afterEach(transition => {
 //NProgress.done();
